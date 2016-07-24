9a4a6f506a110f16eb29523e08f4131df942e44d
葵中剑 <iifksp@swordair.com>
2016-07-24 10:30:44 +0800

    add backtop, add icon, update fontawesome version, archive style, bug fix

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,13 +15,77 @@ hljs.initHighlightingOnLoad();
         $("#nav-drawer").click(function(){
             $('#wrapper').toggleClass("drawer-open");
         })
+		
+		/* archive index */
+		var titleIdx = $('.post-content h2');
+		if(titleIdx.length > 5){
+			var anchor;
+			var indexBox = '<div class="archive-index"><ul>';
+			for(var i = 0; i < titleIdx.length; i++){
+				if(titleIdx.eq(i).attr('id')){
+					anchor = titleIdx.eq(i).attr('id');
+				}else{
+					anchor = "archive-title-index-" + i
+					titleIdx.eq(i).attr('id').attr('id', anchor);
+				}
+
+				indexBox += '<li id="nav-' + anchor + '"><a href="#' + anchor + '">' + titleIdx.eq(i).html().trim() + '</a></li>';
+			}
+			indexBox += '</ul></div>';
+			$('.post-content').prepend(indexBox);
+			$('.archive-index li a').on('click', function(){
+				var $this = $(this);
+				$(document).scrollTop($($this.attr('href')).offset().top - 65);
+				
+				//$('html, body').animate({
+				//	scrollTop: $($this.attr('href')).offset().top - 65
+				//}, 1000);
+				
+				return false;
+			});
+			
+			var curTit;
+			var preTit = titleIdx.eq(0);
+			$('#nav-' + preTit.attr('id')).addClass('active');
+			
+			$(document).on('scroll', function(){
+				
+				titleIdx.each(function(){
+					var $this = $(this);
+					if($(document).scrollTop() > $this.offset().top - 300){
+						curTit = $this;
+					}
+					
+				});
+				
+				$('#nav-' + preTit.attr('id')).removeClass('active');
+				$('#nav-' + curTit.attr('id')).addClass('active');
+				preTit = curTit;
+			});
+			
+			
+		}
     });
 	
-	$(document).scroll(function(){
+	$(".backtop .lnk").on("click", function(){
+		$(document).scrollTop(0);
+		return false;
+	});
+	
+	$(document).on('scroll', function(){
 		if((document.documentElement.scrollTop || document.body.scrollTop) > 30){
 			$('.site-head').addClass('site-head-fixed');
 		}else{
 			$('.site-head').removeClass('site-head-fixed');
-		}	
+		}
+		
+		if((document.documentElement.scrollTop || document.body.scrollTop) > 400){
+			$('.backtop').fadeIn(300);
+		}else{
+			$('.backtop').fadeOut(300);
+		}
     })
+	
+	
+	
 }(jQuery));
